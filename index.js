8e9e499edb65c587dc4c7f1075e64b7a3de81cdb
Eugen Rud <eugen.rud@ssa.group>
2020-03-03 10:27:42 +0200

    fixed bugs on scroll event

--- index.js
+++ index.js
@@ -28,62 +28,55 @@ function navigationHandler() {
     } else {
       $('body').addClass('open-navigation');
       addHeaderOverlay();
-      addInlineDuration( $('.navigation-list-item'), 'shown' );
+      setTimeout( function () {
+        addInlineDuration( $('.navigation-list-item'), 'shown' );
+      }, 300)
     }
   });
 }
+
 
 function addInlineDuration(lists, classAdd) {
+
   for (let i = 1; i <= lists.length + 1; i++) {
     var animationDelay = ( i + i ) <= 9 ? '0.' + ( i + i ) : ( i + i )  / 10 ;
-    setTimeout(function () {
-      $(lists[i-1]).attr('data-item', i);
-      ($(lists[i-1]).addClass(classAdd));
-    }, (i * 150) );
+    $(lists[i-1]).css('transition-delay', animationDelay + 's');
   }
+  lists.addClass(classAdd)
 }
 
-function navigationAmimationHandler() {
-  $('.navigation-link_dropdown-slide a').on('click', function (e) {
+function navigationBtnBackHandler() {
+  $('.navigation-sub_back').on('click', function (e) {
     e.stopPropagation();
+    var t = $(this).closest('.navigation-link_dropdown-slide').find('li:last-child').css('transition-delay').split('s')[0] * 1000 + 200;
+    $(this).closest('.navigation-list-item').removeClass('open-sub');
+
+    setTimeout( function () {
+      $('.navigation').removeClass('bg-primary');
+      $('.navigation-list').removeAttr('style');
+      $('.navigation-list-item').removeClass('notActive').addClass('shown');
+    }, t)
   });
+}
 
-  $('.navigation-list-item').on('click', function (e) {
+function navigationAnimationHandler() {
+  $('.navigation-list-item_dropdown').on('click', function () {
+    var that = this;
+    var thatListHeight = $(that).find('.navigation-link_dropdown-slide').height();
 
-    if ($(this).hasClass('navigation-list-item_dropdown')) {
-      e.stopPropagation();
-      var thatId = $(this).attr('data-item');
-      $('.navigation-list-item').each( function (i, el) {
+    $('.navigation-list-item').removeClass('shown');
 
-        setTimeout( function () {
-          if ($(el).attr('data-item') !== thatId ) {
-            $(el).toggleClass('shown');
-          } else {
+    var t = $('.navigation-list-item:last-child').css('transition-delay').split('s')[0] * 1000 + 200;
+    setTimeout(function () {
+      $('.navigation-list-item').addClass('notActive');
+      $('.navigation-list').css('height', thatListHeight + 'px');
+      $('.navigation').addClass('bg-primary');
 
-            if ($(el).hasClass('open-sub')) {
-              $(el).find('.navigation-link_dropdown-slide li').each( function (i, el) {
-                setTimeout(function () {
-                  $(el).removeClass('open-sub')
-                }, (i+1) * 100)
-              });
-              $('.navigation').removeClass('bg-primary');
-              $(el).removeClass('open-sub');
-            } else {
-
-              if ( $(el).hasClass('navigation-list-item_dropdown')) {
-                setTimeout( function () {
-                  addInlineDuration($(el).find('.navigation-link_dropdown-slide li'), 'open-sub');
-                  $(el).addClass('open-sub');
-                  $('.navigation').addClass('bg-primary');
-                }, $('.navigation-list-item').length * 100)
-              }
-
-            }
-          }
-        },  (i +1) * 100)
-      })
-    }
-
+      setTimeout( function () {
+        addInlineDuration( $(that).find('.navigation-link_dropdown-slide li'), '');
+        $(that).addClass('open-sub');
+      }, 0)
+    }, t )
   });
 }
 
@@ -105,7 +98,7 @@ function removeHeaderOverlay() {
     $('#header').find('.overlay').removeClass('visible');
     $('#header').find('.open-sub').removeClass('open-sub');
     $('.navigation').removeClass('bg-primary');
-    $('.navigation-list-item').removeClass('shown');
+    $('.navigation-list-item').removeAttr('style').removeClass('shown notActive');
     setTimeout(function () {
       $('#header').find('.overlay').remove();
     }, 500)
@@ -173,18 +166,20 @@ function transformDataYearText(text) {
 }
 
 function addFixedBehaviorHandler(el, wrapHeight, number) {
-  el.wrap(`<div class="wrap" style="height: ${wrapHeight}px; "></div>`);
-  el.attr('style', `top: ${headerHeight}px `).addClass('fixed');
-
+  var wrapBlock = `<div class="wrap" style="height: ${wrapHeight}px; "></div>`;
+  el.wrap(wrapBlock);
+  el.css('top', headerHeight + 'px').addClass('fixed');
   if (number) {
     currI = number;
   }
-
 }
 
 function removeFixedBehaviorHandler(el, number) {
   el.removeClass('fixed');
-  el.unwrap();
+  setTimeout(function () {
+    el.unwrap();
+  }, 50);
+
   currI = number;
 }
 
@@ -228,22 +223,40 @@ $(window).scrollEnd(function(){
 function timelinePageScrollFunctionality() {
 
   if ($('.timeline-section_large').length) {
+
+    $('.timeline-section_large').each( function (i, el) {
+      if ( $(el).offset().top <=  $(window).scrollTop() + headerHeight ) {
+        currI = i;
+      }
+    });
+
+    if (currI !== 0) {
+      var currentYear =  $(`[data-target='#${$($('.timeline-section_large')[currI]).attr('id')}' ]`).attr('data-year');
+      var currentNumber =  $(`[data-target='#${$($('.timeline-section_large')[currI]).attr('id')}' ]`).attr('data-number');
+      var prevNumber = $(`[data-target='#${$($('.timeline-section_large')[currI - 1]).attr('id')}' ]`).attr('data-number');
+
+      addFixedBehaviorHandler($($('.timeline-section_large')[currI]),$($('.timeline-section_large')[currI])[0].offsetHeight + headerHeight );
+      $('.stats-bar').addClass('visible');
+      countStatsBarNumber( prevNumber , currentNumber, transformDataYearText(currentYear));
+
+    }
+
     $(document).on('scrollUp',function(){
       var elemList = $('.timeline-section_large');
-      var $lastItem = $( $('.timeline-section_large')[ $('.timeline-section_large').length -1 ]);
+      var $lastItem = $( $('.timeline-section_large')[ $('.timeline-section_large').length - 1 ]);
       var $lastItemTargetBlock = $(`[data-target='#${ $lastItem.attr('id') }' ]`) ;
       var scrollHeight = $(window).scrollTop() + headerHeight;
 
       elemList.each( function (i, el) {
         var $el = $(el);
-        var wrapHeight = $el[0].offsetHeight + headerHeight;
+
+        var windowHeight = $(window).height() - headerHeight;
         var $currentEl = $($('.timeline-section_large')[currI]);
         var dataCurrent =  `[data-target='#${$currentEl.attr('id')}' ]`;
         var $currentElByDataTarget = $(dataCurrent);
-        var startPoint = ( $currentElByDataTarget.offset().top ) + $( window ).height() + headerHeight;
-        var endPoint = ( $currentElByDataTarget.offset().top ) - $( window ).height() + headerHeight;
-
-        if ($el.hasClass('fixed') && $el.parent().offset().top >=  scrollHeight) {
+        var startPoint = ( $currentElByDataTarget.offset().top + $currentElByDataTarget.height() + (headerHeight * 2 ));
+        var endPoint = ( $currentElByDataTarget.offset().top ) - windowHeight;
+        if ($el.hasClass('fixed') && $el.parent().offset().top >=  scrollHeight ) {
 
           if ( i === 0 ) {
             removeFixedBehaviorHandler($el, i);
@@ -251,20 +264,17 @@ function timelinePageScrollFunctionality() {
             countStatsBarNumber( $($('.stats-bar .stats-bar-counter')[0]).text() , 0);
           } else {
             removeFixedBehaviorHandler($el, i - 1);
-            addFixedBehaviorHandler($($('.timeline-section_large')[currI]), wrapHeight);
-            var dataTarget =`[data-target='#${$($('.timeline-section_large')[currI]).attr('id')}' ]` ;
-            countStatsBarNumber( $($('.stats-bar .stats-bar-counter')[0]).text() , $(dataTarget).attr('data-number'), transformDataYearText($(dataTarget).attr('data-year')));
+            addFixedBehaviorHandler($(elemList[currI]), elemList[currI].offsetHeight );
+            var dataTarget =`[data-target='#${$(elemList[currI]).attr('id')}' ]` ;
+            countStatsBarNumber( $($('.stats-bar .stats-bar-counter')[0]).text() ,
+              $(dataTarget).attr('data-number'),
+              transformDataYearText($(dataTarget).attr('data-year')));
           }
         }
 
-        if ( currI === $('.timeline-section_large').length -1
-          && scrollHeight <= $lastItemTargetBlock.offset().top + $lastItemTargetBlock.height() ) {
-          $('.stats-bar').addClass('visible');
-        }
+        if ( scrollHeight <= startPoint && currI !== elemList.length - 1) {
 
-        if ( scrollHeight <= startPoint) {
-
-          var percentOpacity = ( scrollHeight - endPoint )  / (startPoint - endPoint );
+          var percentOpacity = ( scrollHeight - endPoint  )  / (startPoint - endPoint - $el.parent().find('.counter-years_large').height() );
 
           if ( (1 - percentOpacity ) < 0 ) {
             $currentEl.css('opacity', 0);
@@ -275,27 +285,37 @@ function timelinePageScrollFunctionality() {
           }
         }
 
+        if ( currI === elemList.length - 1
+          && scrollHeight <= $lastItemTargetBlock.offset().top + ($lastItemTargetBlock.height() / 2) ) {
+          $('.stats-bar').addClass('visible');
+
+          if ( scrollHeight <= startPoint ) {
+            var percentOpacity = ( scrollHeight - endPoint )  / (startPoint - endPoint );
+            $currentEl.css('opacity', (1 - percentOpacity ));
+          }
+        }
+
       });
 
     });
-    $(document).on('scrollDown',function(){
+    $(document).on('scrollDown',function() {
+      var elemList = $('.timeline-section_large');
 
-      $('.timeline-section_large').each( function (i, el) {
+      elemList.each( function (i, el) {
         var $el = $(el);
-        var toCenterEl = ($el.innerHeight() / 2) + headerHeight;
-        var elementOffset = $el.offset().top;
-        var wrapHeight = $el[0].offsetHeight;
+        var $elParent = $el.parent();
+        var elementOffset = $el.offset().top ;
+        var elementHeight = $el.height();
         var data = `[data-target='#${$(el).attr('id')}' ]`;
         var $elByDataTarget = $(data);
         var elByDataTargetHeight = $elByDataTarget.height();
-        var startPoint = ( $elByDataTarget.offset().top ) - $( window ).height() - (elByDataTargetHeight / 1) ;
-        var scrollHeight = $(window).scrollTop() + headerHeight;
+        var startPoint = ( $elByDataTarget.offset().top ) - $( window ).height();
+        var scrollHeight = ($(window).scrollTop() + headerHeight);
 
+        if ( elementOffset  <= scrollHeight ) {
 
-        if ( elementOffset <=  scrollHeight ) {
-
-          if ( !$el.parent().hasClass('wrap') && i > currI ) {
-            addFixedBehaviorHandler($el, wrapHeight, i);
+          if ( !$elParent.hasClass('wrap') && i > currI ) {
+            addFixedBehaviorHandler($el, elementHeight, i);
             countStatsBarNumber( $($('.stats-bar .stats-bar-counter')[0]).text() , $elByDataTarget.attr('data-number'), transformDataYearText($elByDataTarget.attr('data-year')));
 
           } else if ( !$el.parent().hasClass('wrap') && i === currI ) {
@@ -304,21 +324,24 @@ function timelinePageScrollFunctionality() {
               $('.stats-bar').addClass('visible');
             }
 
-            addFixedBehaviorHandler($el, wrapHeight, i);
+            addFixedBehaviorHandler($el, elementHeight, i);
             countStatsBarNumber( $($('.stats-bar .stats-bar-counter')[0]).text() , $elByDataTarget.attr('data-number'), transformDataYearText($elByDataTarget.attr('data-year')));
 
           } else {
 
             if ( currI > 0)  {
-              if ($($('.timeline-section_large')[currI-1]).hasClass('fixed') && $($('.timeline-section_large')[currI-1]).parent().hasClass('wrap') ) {
-                $($('.timeline-section_large')[currI-1]).removeClass('fixed');
-                $($('.timeline-section_large')[currI-1]).unwrap();
+              if ($(elemList[currI-1]).hasClass('fixed') && $(elemList[currI-1]).parent().hasClass('wrap') ) {
+                $(elemList[currI-1]).removeClass('fixed');
+                $(elemList[currI-1]).unwrap();
               }
             }
           }
 
           if ( scrollHeight >= startPoint) {
-            var percentOpacity = (scrollHeight - startPoint ) / ( ($elByDataTarget.offset().top + elByDataTargetHeight) -  startPoint ) * 100 ;
+
+            var percentOpacity = ( ( (scrollHeight) - startPoint )
+              / ( ($elByDataTarget.offset().top + elByDataTargetHeight)
+                -  startPoint - ($elParent.find('.counter-years_large').height() / 2 ) ) * 100 ) ;
 
             if ( ((100 - percentOpacity) / 100) < 0 ) {
               $el.css('opacity', 0);
@@ -328,17 +351,18 @@ function timelinePageScrollFunctionality() {
               $el.css('opacity', (100 - percentOpacity) / 100);
             }
 
-            if ( currI === $('.timeline-section_large').length -1) {
+            if ( currI === elemList.length - 1) {
 
-              var $lastItem = $(`[data-target='#${  $($('.timeline-section_large')[$('.timeline-section_large').length -1]).attr('id') }' ]`);
+              var $lastItem = $(`[data-target='#${  $(elemList[elemList.length - 1]).attr('id') }' ]`);
 
-              if ( scrollHeight >= $lastItem.offset().top + $( window ).height() + headerHeight ) {
+              if ( scrollHeight >= $lastItem.offset().top + ($lastItem.height() / 2) ) {
                 $('.stats-bar').removeClass('visible');
+                $el.css('opacity', 0)
               }
             }
           }
         }
-      })
+      });
 
     });
   }
@@ -347,8 +371,9 @@ function timelinePageScrollFunctionality() {
 
 $(document).ready( function () {
   navigationHandler();
-  navigationAmimationHandler();
+  navigationAnimationHandler();
   timelinePageScrollFunctionality();
+  navigationBtnBackHandler();
 
   windowOnResizeHandler();
 
